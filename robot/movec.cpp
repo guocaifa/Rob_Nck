2c6aaac157609bd4bd1dd6ed263c0336585d1bee
guocaifa <892028458@qq.com>
2018-05-07 23:15:44 +0800

    201805072315

--- robot/movec.cpp
+++ robot/movec.cpp
@@ -24,11 +24,34 @@
 
 #include "movec.h"
 
-static void GetOMatrix(point *pCenter, point *pCrn, point *pMid, point *pTrg);
-static void GetCycleO(point *pO, const point *pCrn, const point *pMid, const point *pEnd);
+static void GetCycleProperty(cycle *pCycle, point *pCrn, point *pMid, point *pTrg);
 
-extern void MoveC(coorquat *pCrn, coorquat *pMid, coorquat *pTgt, double Vmax, double Acc)
+extern void MoveC(cooreuler *pCrn, cooreuler *pMid, cooreuler *pTgt, double Vmax, double Acc)
 {
+  double TMax;
+  point  PCrn;
+
+  cycle Property;
+  memset(&Property, 0, sizeof(cycle));
+
+  GetCycleProperty(&Property, pCrn->xCoor, pMid->xCoor, pTgt->xCoor);
+
+  Vmax = Vmax / Property.xR;/* 线速度->角速度 */
+
+  if(Property.xCycDir){
+    PCrn.Val[0] = Property.xR * cos(-Vmax * Tmax);
+    PCrn.Val[1] = Property.xR * sin(-Vmax * Tmax);;
+    PCrn.Val[2] = 0;
+  }
+  else{
+    PCrn.Val[0] = Property.xR * cos(Vmax * Tmax);
+    PCrn.Val[1] = Property.xR * sin(Vmax * Tmax);;
+    PCrn.Val[2] = 0;
+  }
+
+  /* 转化到工作坐标系 */
+
+  return;
 }
 
 /* 函数说明；求圆弧的圆心
@@ -47,7 +70,7 @@ static void GetCycleO(point *pO, const point *pCrn, const point *pMid, const poi
   return;
 }
 
-static void GetOMatrix(point *pCenter, point *pCrn, point *pMid, point *pTrg)
+static void GetCycleProperty(cycle *pCycle, point *pCrn, point *pMid, point *pTrg)
 {
   double k11,k12,k13,\k14, \
          k21,k22,k23,\k24, \
@@ -57,7 +80,17 @@ static void GetOMatrix(point *pCenter, point *pCrn, point *pMid, point *pTrg)
          x2 = pMid->Val[0], y2 = pMid->Val[1], z2 = pMid->Val[2], \
          x3 = pTrg->Val[0], y3 = pTrg->Val[1], z3 = pTrg->Val[2]; \
 
+  double x0,y0,z0;  //圆心坐标
+
   double det;
+  double row1,row2,row3;
+  double _k;
+  double X1,Y1,Z1,X2,Y2,Z2;
+  double p21x = x2 - x1,\
+         p21y = y2 - y1,\
+         p31x = x3 - x1,\
+         p31y = y3 - y1;\
+  double AngP1OP2,AngP1OP3;
 
   k11 = (y1 - y3) * (z2 - z3) - (y2 - y3) * (z1 - z3);
   k12 = (x2 - x3) * (z1 - z3) - (x1 - x3) * (z2 - z3);
@@ -75,11 +108,65 @@ static void GetOMatrix(point *pCenter, point *pCrn, point *pMid, point *pTrg)
   k34 = -((x3 * x3 - x2 * x2) + (y3 * y3 - y2 * y2) + (z3 * z3 - z2 * z2)) / 2;
 
   det = k11 * k22 * k33 + k31 * k12 * k23 + k21 * k32 * k13 - \
-        k13 * k22 * k31 - k12 * k21 * k33 - k32 * k23 * k11;
+        k13 * k22 * k31 - k12 * k21 * k33 - k32 * k23 * k11;/* 逆矩阵的行列式 */
 
-  pCenter->Val[0] = ;
-  pCenter->Val[1] = ;
-  pCenter->Val[2] = ;
+  row1 = -k14 * (k22 * k33 - k23 * k32) + k24 * (k12 * k33 - k13 * k32) - k34 * (k12 * k23 - k22 * k13);
+
+  row2 =  k14 * (k21 * k33 - k23 * k31) - k24 * (k11 * k33 - k31 * k13) + k34 * (k11 * k23 - k21 * k13);
+
+  row3 = -k14 * (k21 * k32 - k31 * k22) + k24 * (k11 * k32 - k31 * k12) - k34 * (k11 * k22 - k21 * k12);
+
+  pCycle->xCenter.Val[0] = x0 =  row1 / det;
+  pCycle->xCenter.Val[1] = y0 =  row2 / det;
+  pCycle->xCenter.Val[2] = z0 =  row3 / det;
+
+  pCycle->xR = sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0) + (z1 - z0) * (z1 - z0));
+
+  /* n1 */
+  pCycle->xTran[0][0] = X2 = (x1 - x0) / pCycle->xR;
+  pCycle->xTran[1][0] = Y2 = (y1 - y0) / pCycle->xR;
+  pCycle->xTran[2][0] = Z2 = (z1 - z0) / pCycle->xR;
+  pCycle->xTran[3][0] = 0;
+  /* a1 */
+  _k = sqrt(k11 * k11 + k12 * k12 + k13 * k13);
+  pCycle->xTran[0][2] = X1 = k11 / _k;
+  pCycle->xTran[1][2] = Y1 = k12 / _k;
+  pCycle->xTran[2][2] = Z1 = k13 / _k;
+  pCycle->xTran[3][2] = 0;
+
+  /* o1 = a1 * n1 */
+  pCycle->xTran[0][1] = Y1 * Z2 - Y2 * Z1;
+  pCycle->xTran[1][1] = Z1 * X2 - Z2 * X1;
+  pCycle->xTran[2][1] = X1 * Y2 - X2 * Y1;
+  pCycle->xTran[3][1] = 0;
+
+  /* p0 */
+  pCycle->xTran[0][3] = x1;
+  pCycle->xTran[1][3] = y1;
+  pCycle->xTran[2][3] = z1;
+  pCycle->xTran[3][3] = 1;
+
+  if(atan2(p21y,p21x) < 0)  AngP1OP2 = atan2(p21y,p21x) + 2 * PI;
+  else                      AngP1OP2 = atan2(p21y,p21x);
+
+  if(atan2(p31y,p31x) < 0)  AngP1OP3 = atan2(p31y,p31x) + 2 * PI;
+  else                      AngP1OP3 = atan2(p31y,p31x);
+
+  if(AngP1OP2 < AngP1OP3){
+    pCycle->xCycDir = false;
+    pCycle->xAngle  = AngP1OP3;
+  }
+  else{
+    pCycle->xCycDir = true;
+    pCycle->xAngle  = 2 * PI - AngP1OP3;
+  }
 
   return;
+}
+
+static void __inline MatDet(double *pDetVal, point *pCol1, point *pCol2, point *pCol3)
+{
+  double det;
+
+  *pDetVal = det;
 }
--- robot/movec.h
+++ robot/movec.h
@@ -30,6 +30,15 @@
 #include "robot.h"
 #include "linuxtypes.h"
 
+typedef struct
+{
+  point     xCenter; //圆心
+  double    xR;      //半径
+  bool      xCycDir; //圆弧方向  true:顺时针  false:逆时针
+  double    xAngle;  //运动的角度大小
+  matrix4_4 xTran;   //矩阵转化
+}cycle;
+
 extern void MoveC(coorquat *pCrn, coorquat *pMid, coorquat *pTgt, double Vmax, double Acc);
 
 #endif // MOVEC_H
